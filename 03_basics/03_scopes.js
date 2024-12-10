69017ccf2962d97e76c6029c7ab63c2fedd19f1a
Aayush <ayushsakpal007@gmail.com>
2024-12-10 10:44:56 +0530

    Scopes basics

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -1,13 +1,16 @@
 var c = 400;
+let a = 100;
 
 if(true) {
     let a = 10;
     const b = 20;
     c = 30;
     var d = 40;
+
+    console.log("Value of a inside block =", a); // Will print 10
 }
 
-// console.log(a); // Will give error
+console.log("Value of a outside block =", a); // Will print 100
 // console.log(b); // Will give error
 console.log(c); // Will NOT give error! Value of c will be printed
 console.log(d); // Will NOT give error! Value of d will be printed
