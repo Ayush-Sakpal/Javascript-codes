10adbcf707eefeaa1d681da1006dba08ef7a4c41
Aayush <ayushsakpal007@gmail.com>
2024-12-10 10:58:39 +0530

    closure basics

---
+++ 03_basics/04_scopes.js
@@ -0,0 +1,20 @@
+function one() {
+    let username = "Aayush";
+
+    function two() {
+        let application = "MyApp";
+
+        console.log("username in function two:", username);
+        console.log("Application in function two:", application);
+    }
+
+    console.log("USername in function one:", username);
+    // console.log("Application in function one:", application); // Gives error
+
+    two();
+}
+
+one();
+
+// Inner block can access variables of the outer block
+// Outer block CANNOT access variables of the inner block
