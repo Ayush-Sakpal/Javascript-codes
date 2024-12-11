052436def1b65040f385495dec3b8fc531d17590
Aayush <ayushsakpal007@gmail.com>
2024-12-11 14:17:45 +0530

    falsy and some truthy values

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,25 @@
+/*
+    Falsy Values:
+
+    1. false
+    2. 0
+    3. -0
+    4. ""
+    5. BigInt 0n
+    6. null
+    7. undefined
+    8. NaN
+
+    Remaining all are truthy values
+*/
+
+/*
+    Some of the truthy values:
+
+    1. "0"
+    2. "false"
+    3. " " -> String containing a space
+    4. [] -> empty array
+    5. {} -> empty object
+    6. function() {} -> empty function
+*/
