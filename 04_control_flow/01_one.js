523b99b536283d5b4a9734157303caee4feee494
Aayush <ayushsakpal007@gmail.com>
2024-12-11 14:03:14 +0530

    if-else basics

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,13 @@
+if(2 == "2"){
+    console.log("Executed 2 == '2'!");
+}
+else {
+    console.log("NOT executed 2 == '2'!")
+}
+
+if(2 === "2") {
+    console.log("Executed 2 === '2' !")
+}
+else{
+    console.log("NOT executed 2 === '2' !")
+}
