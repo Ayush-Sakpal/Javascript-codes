75d84e851b99595a947c08e2078bf03d4df9826e
Aayush <ayushsakpal007@gmail.com>
2024-12-11 14:23:16 +0530

    Null Coalescing Operator

--- 04_control_flow/03_truthy.js
+++ 04_control_flow/03_truthy.js
@@ -23,3 +23,14 @@
     5. {} -> empty object
     6. function() {} -> empty function
 */
+
+// Null Coalescing Operator (??) : safety check for null, undefined
+const val1 = 5 ?? 10;
+const val2 = null ?? 30;
+const val3 = undefined ?? 40;
+const val4 = null ?? 50 ?? 60;
+
+console.log("Val 1 =", val1);
+console.log("Val 2 =", val2);
+console.log("Val 3 =", val3);
+console.log("Val 4 =", val4);
