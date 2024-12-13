ddb416b3410540e1a33aa6dc4a50198c72caae3f
Aayush <ayushsakpal007@gmail.com>
2024-12-13 10:59:27 +0530

    maps basics

---
+++ 05_iterations/03_map.js
@@ -0,0 +1,5 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Works similar to foreach loop but returns values.
+const newNumns = nums.map( (num) => {return num + 10});
+console.log(newNumns);
